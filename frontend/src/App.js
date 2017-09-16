148133480289aac8675f7a98e275ff1eb308eaed
Ivan Vasiljevic <robince885@gmail.com>
2017-09-17 00:57:22 +0200

    Improve client page so user can easily find difference if they are logged in or not

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,23 +6,51 @@ class App extends Component {
   constructor() {
     super();
 
-    this.onFailed = this.onFailed.bind(this);
-    this.onSuccess = this.onSuccess.bind(this);
+    this.state = { isAuthenticated: false, user: null, token: ''};
   }
 
-  onSuccess(body) {
-    alert(JSON.stringify(body));
-  }
+  onSuccess = (response) => {
+    const token = response.headers.get('x-auth-token');
+    response.json().then(user => {
+      if (token) {
+        this.setState({isAuthenticated: true, user: user, token: token});
+      }
+    });
+  };
 
-  onFailed(error) {
+  onFailed = (error) => {
     alert(error);
-  }
+  };
+
+  logout = () => {
+    this.setState({isAuthenticated: false, token: '', user: null})
+  };
 
   render() {
+    let content = !!this.state.isAuthenticated ?
+      (
+        <div>
+          <p>Authenticated</p>
+          <div>
+            {this.state.user.email}
+          </div>
+          <div>
+            <button onClick={this.logout} className="button" >
+              Log out
+            </button>
+          </div>
+        </div>
+      ) :
+      (
+        <TwitterLogin loginUrl="http://localhost:4000/api/v1/auth/twitter"
+                      onFailure={this.onFailed} onSuccess={this.onSuccess}
+                      requestTokenUrl="http://localhost:4000/api/v1/auth/twitter/reverse"/>
+      );
+
     return (
-      <TwitterLogin loginUrl="http://localhost:4000/api/v1/auth/twitter"
-        onFailure={this.onFailed} onSuccess={this.onSuccess}
-        requestTokenUrl="http://localhost:4000/api/v1/auth/twitter/reverse"/>
+      <div className="App">
+        {content}
+      </div>
     );
   }
 }
